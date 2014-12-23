c703097c3fb75fb5b6e62c004f755bbf0e01958f
Adrian Vatchinsky <avatchinsky@outlook.com>
2014-12-23 01:08:55 -0500

    added contributor object to the admin page

--- sourceofstuff/contributors/admin.py
+++ sourceofstuff/contributors/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Contributor
 
-# Register your models here.
+
+class ContributorAdmin(admin.ModelAdmin):
+    pass
+admin.site.register(Contributor, ContributorAdmin)

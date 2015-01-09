51692379ca0f6fa9a0b98c47e4d1c1ba2dace9d8
Adrian Vatchinsky <avatchinsky@outlook.com>
2015-01-09 00:33:49 -0500

    wsgi file

--- sourceofstuff/sourceofstuff/wsgi.py
+++ sourceofstuff/sourceofstuff/wsgi.py
@@ -8,6 +8,8 @@ https://docs.djangoproject.com/en/1.7/howto/deployment/wsgi/
 """
 
 import os
+import sys
+sys.path.append('/home/sourceofstuff/django_project/SourceOfStuff/sourceofstuff')
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "sourceofstuff.settings")
 
 from django.core.wsgi import get_wsgi_application
